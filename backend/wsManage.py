9b37cfb2e1dd3eb58677761c5ef9d814cec02824
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2025-12-13 14:52:38 +0200

    Redis replaced the need for wsManage

--- backend/wsManage.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from fastapi import WebSocket
-
-connected_clients: List[WebSocket] = []
-
-async def broadcast(event_type, data):
-    message = {"type": event_type, "data": data}
-    for ws in connected_clients:
-        await ws.send_json(message)
